3ee2062cc682ecf0f97b51595a0092afe4bcf1f6
arnoson <hallo@arno-schlipf.info>
2023-05-16 15:48:37 +0200

    chore: initial commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <NuxtErrorBoundary>
+      <MyComponent />
+      <template #error="{error}">{{ error }}</template>
+    </NuxtErrorBoundary>
+  </div>
+</template>
---
+++ components/MyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button @click="throwNormalError">Throw normal error</button>
+    <button @click="throwMutationError">Throw mutation error</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useMutation } from '@tanstack/vue-query';
+
+const throwNormalError = () => {
+  throw createError('normal error')
+}
+
+const mutation = useMutation(() => {
+  throw createError('mutation error')
+}, { useErrorBoundary: true })
+
+const throwMutationError = () => mutation.mutate()
+</script>
