8fd9e5d39aaf8ae30953a53f10741b91774cb6c5
arnoson <hallo@arno-schlipf.info>
2023-05-25 12:30:06 +0200

    chore: add custom error boundary

--- app.vue
+++ app.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
+    <h2>With NuxtErrorBoundary</h2>
     <NuxtErrorBoundary>
       <MyComponent />
       <template #error="{error}">{{ error }}</template>
     </NuxtErrorBoundary>
+    <hr />
+    <h2>With Custom Error Boundary</h2>
+    <MyErrorBoundary>
+      <MyComponent />
+    </MyErrorBoundary>
   </div>
 </template>
---
+++ components/MyErrorBoundary.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="hasError">{{ error }}</div>
+  <div v-else>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+const hasError = ref(false)
+const error = ref<Error | undefined>()
+
+onErrorCaptured((capturedError) => {
+  hasError.value = true
+  error.value = capturedError
+})
+
+</script>
